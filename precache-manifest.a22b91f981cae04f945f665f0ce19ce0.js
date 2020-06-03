279dbc1cb9063bfe970060a789bc4e8145ed342a
neeka <neekaishappy@gmail.com>
2020-06-03 16:34:15 -0700

    Updates

---
+++ precache-manifest.a22b91f981cae04f945f665f0ce19ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e360423b970f079d0af794820eb7b10d",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "1c2a50ac1356831b3360",
+    "url": "/fcc-pomodoro-clock/static/css/main.aa57e5ab.chunk.css"
+  },
+  {
+    "revision": "4a71c14c16f980d44624",
+    "url": "/fcc-pomodoro-clock/static/js/2.1ef4d8d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fcc-pomodoro-clock/static/js/2.1ef4d8d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2a50ac1356831b3360",
+    "url": "/fcc-pomodoro-clock/static/js/main.3fc375c0.chunk.js"
+  },
+  {
+    "revision": "0d4a4233117511c33496",
+    "url": "/fcc-pomodoro-clock/static/js/runtime-main.c6a4e24b.js"
+  }
+]);
